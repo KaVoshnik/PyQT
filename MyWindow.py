7f6e2b73ecf5a3de119ab7e3a3d4d66b9416fb33
KaVoshnik <hikkan008@gmail.com>
2024-10-16 07:49:27 +0000

    modified:   MyWindow.py 	deleted:    main.py

--- MyWindow.py
+++ MyWindow.py
@@ -1,10 +1,13 @@
 from PyQt5 import QtCore, QtWidgets
 import sys
+import datetime
 
 class MyWindow(QtWidgets.QWidget):
+
     def __init__(self, parent=None):
+        x = datetime.datetime.now()
         QtWidgets.QWidget.__init__(self, parent)
-        self.label = QtWidgets.QLabel("Привет, мир!")
+        self.label = QtWidgets.QLabel(x.strftime("%x"))
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.btnQuit = QtWidgets.QPushButton("&Закрыть")
         self.vbox = QtWidgets.QVBoxLayout()
@@ -16,8 +19,7 @@ class MyWindow(QtWidgets.QWidget):
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
-    window.setWindowTitle("ООП-стиль создания окна")
+    window.setWindowTitle("Data time")
     window.resize(300, 70)
     window.show()
     sys.exit(app.exec_())
-
