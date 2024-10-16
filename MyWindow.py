08b370dc6f0c345bf08ce927ce56ff37bcb64f2f
KaVoshnik <hikkan008@gmail.com>
2024-10-16 07:52:00 +0000

    modified:   MyWindow.py

--- MyWindow.py
+++ MyWindow.py
@@ -1,25 +1,31 @@
-from PyQt5 import QtCore, QtWidgets
 import sys
+from PyQt5 import QtWidgets, QtCore
 import datetime
 
 class MyWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
-    def __init__(self, parent=None):
-        x = datetime.datetime.now()
-        QtWidgets.QWidget.__init__(self, parent)
-        self.label = QtWidgets.QLabel(x.strftime("%x"))
+        self.label = QtWidgets.QLabel()
         self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.btnQuit = QtWidgets.QPushButton("&Закрыть")
-        self.vbox = QtWidgets.QVBoxLayout()
-        self.vbox.addWidget(self.label)
-        self.vbox.addWidget(self.btnQuit)
-        self.setLayout(self.vbox)
-        self.btnQuit.clicked.connect(QtWidgets.qApp.quit)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.label)
+
+        self.setLayout(self.layout)
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)  # update every 1 second
+
+    def update_time(self):
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        self.label.setText(current_time)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
-    window.setWindowTitle("Data time")
-    window.resize(300, 70)
+    window.setWindowTitle("Current Time")
+    window.resize(300, 100)
     window.show()
     sys.exit(app.exec_())
